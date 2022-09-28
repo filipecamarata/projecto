b7f8fbc521b5f779e6d8844a689b1acb189fe825
Filipe Camarata <filipeedvandro942@gmail.com>
2022-09-28 19:30:40 +0100

    Alteração da cor dos links do cabeçalho

--- cabecalho.php
+++ cabecalho.php
@@ -19,13 +19,13 @@
         </header>               
 		<div class="nav navbar-nav">        	
             <nav>
-                <button type="button" class="btn btn-link"><a class="link-cabecalho" href="index.php">Home</a></button>
-                <button type="button" class="btn btn-link"><a class="link-cabecalho" href="destaques.php">Destaques</a></button>
-                <button type="button" class="btn btn-link"><a class="link-cabecalho" href="salao.php">Salões</a></button>
-                <button type="button" class="btn btn-link"><a class="link-cabecalho" href="dj.php">DJs</a></button>
-                <button type="button" class="btn btn-link"><a class="link-cabecalho" href="fotografo.php">Fotográfos</a></button>
-                <button type="button" class="btn btn-link"><a class="link-cabecalho" href="#">Reservas</a></button>
-                <button type="button" class="btn btn-link"><a class="link-cabecalho" href="#">Sobre</a></button>
+                <button type="button"><a class="link-cabecalho" href="index.php">Home</a></button>
+                <button type="button"><a class="link-cabecalho" href="destaques.php">Destaques</a></button>
+                <button type="button"><a class="link-cabecalho" href="salao.php">Salões</a></button>
+                <button type="button"><a class="link-cabecalho" href="dj.php">DJs</a></button>
+                <button type="button"><a class="link-cabecalho" href="fotografo.php">Fotográfos</a></button>
+                <button type="button"><a class="link-cabecalho" href="#">Reservas</a></button>
+                <button type="button"><a class="link-cabecalho" href="#">Sobre</a></button>
             </nav>
 		</div>
 	</div>
