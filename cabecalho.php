c118ea47b11883670f3614b359c89ec827f85fb0
Filipe Camarata <filipeedvandro942@gmail.com>
2022-09-29 14:56:30 +0100

    Melhorei a semântica do cabeçaho

--- cabecalho.php
+++ cabecalho.php
@@ -5,31 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Events-Spaces</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/events-spaces.css">
-
-    
+   
 </head>
 <body>
-<img class="logotipo" src="imagens/logo.png" alt="Logótipo">
-<div class="cabecalho" class="navbar navbar-inverse  navbar-fixed-top">
-	<div class="container">
-        <header class="header">
-            <h1>Events-Spaces</h1>
-        </header>               
-		<div class="nav navbar-nav">        	
-            <nav>
-                <button type="button"><a class="link-cabecalho" href="index.php">Home</a></button>
-                <button type="button"><a class="link-cabecalho" href="destaques.php">Destaques</a></button>
-                <button type="button"><a class="link-cabecalho" href="salao.php">Salões</a></button>
-                <button type="button"><a class="link-cabecalho" href="dj.php">DJs</a></button>
-                <button type="button"><a class="link-cabecalho" href="fotografo.php">Fotográfos</a></button>
-                <button type="button"><a class="link-cabecalho" href="#">Reservas</a></button>
-                <button type="button"><a class="link-cabecalho" href="#">Sobre</a></button>
-            </nav>
-		</div>
-	</div>
-</div>
+<header class="cabecalho">      
+    <img class="logotipo" src="imagens/logo.png" alt="Logótipo">
+    <h1>Events-Spaces</h1>
+    <nav>
+        <a class="link-cabecalho" href="index.php">HOME</a>
+        <a class="link-cabecalho" href="destaques.php">DESTAQUES</a>
+        <a class="link-cabecalho" href="salao.php">SALÕES</a>
+        <a class="link-cabecalho" href="dj.php">DJ</a>
+        <a class="link-cabecalho" href="fotografo.php">FOTOGRÁFOS</a>
+        <a class="link-cabecalho" href="#">RESERVAS</a>
+        <a class="link-cabecalho" href="#">SOBRE</a>
+    </nav>			
+</header> 
     
 <main>
     
