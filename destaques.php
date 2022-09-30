7f6a14a375faa2195e9e07a9069a3c4db3553478
Filipe Camarata <filipeedvandro942@gmail.com>
2022-09-30 18:02:19 +0100

    Melhorei a semântica do Index e do Destaque

--- destaques.php
+++ destaques.php
@@ -1,19 +1,22 @@
 <?php include ("cabecalho.php");?>
-
-<main>
-    <div class="destaque">
-        <h2>Eventos em destaques</h2> 
-        <h3>Fotos Em Destaques</h3>
-        <img src="imagens/foto.png" alt="Imagem em destaque"> <img src="imagens/imagens.png" alt="Imagem em destaque">
-        <img src="imagens/foto.png" alt="Imagem em destaque"> <img src="imagens/foto.png" alt="Imagem em destaque">  
-        <img src="imagens/imagens.png" alt="Imagem em destaque"> <img src="imagens/foto.png" alt="Imagem em destaque">  
+<section>
+    <article class="destaque">       
+        <h2>Fotos Em Destaques</h2>
+        <img src="imagens/foto.png" alt="Imagem em destaque"> 
+        <img src="imagens/imagens.png" alt="Imagem em destaque">
+        <img src="imagens/foto.png" alt="Imagem em destaque"> 
+        <img src="imagens/foto.png" alt="Imagem em destaque">  
+        <img src="imagens/imagens.png" alt="Imagem em destaque"> 
+        <img src="imagens/foto.png" alt="Imagem em destaque">  
   
-        <h3>Videos Em Destaques</h3>
-        <img src="imagens/videos.png" alt="Videos em destaque">   <img src="imagens/videos.png" alt="Videos em destaque">   <img src="imagens/videos.png" alt="Videos em destaque">   <img src="imagens/videos.png" alt="Videos em destaque">   <img src="imagens/videos.png" alt="Videos em destaque">   <img src="imagens/videos.png" alt="Videos em destaque">      
-    
-    </div>
-</main>
-   
-    
-    
+        <h2>Videos Em Destaques</h2>
+        <img src="imagens/videos.png" alt="Videos em destaque">   
+        <img src="imagens/videos.png" alt="Videos em destaque">   
+        <img src="imagens/videos.png" alt="Videos em destaque">   
+        <img src="imagens/videos.png" alt="Videos em destaque">   
+        <img src="imagens/videos.png" alt="Videos em destaque">   
+        <img src="imagens/videos.png" alt="Videos em destaque">    
+    </article>
+</section>
+
 <?php include ("rodape.php");?>
--- index.php
+++ index.php
@@ -1,35 +1,43 @@
 <?php include ("cabecalho.php");?>
    
-    <main>
-        <div class="destaque">
+    <section>
+        <article class="destaque">
             <h2>Destaques</h2>
             <p>Abaixo temos os eventos que se realizaram recentemente. Podes verificar clicando nas imagens e videos.</p>
-            <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"> </a><a href="destaques.php"><img src="imagens/videos.png" alt="Videos em destaques"></a> <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"> </a>
-        </div>
-    </main>
-    
-    <main>
-        <div class="salao">
+            <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"></a>
+            <a href="destaques.php"><img src="imagens/videos.png" alt="Videos em destaques"></a> 
+            <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"></a>
+        </article>   
+    </section>
+        
+    <section>
+        <article  class="salao">
              <h2>Salões Para Eventos</h2>
              <p>Abaixo tem uma variedade de salões que podes reservar, para diferentes tipos de eventos a preços acessiveis.</p>
-             <a href="salao.php"><img src="imagens/foto.png" alt="Salão para reservar"></a> <a href="salao.php"><img src="imagens/salao.png" alt="Salão para reservar"></a> <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"> </a>
-        </div>
-    </main>
-
-    <main>
-        <div class="dj">
+             <a href="salao.php"><img src="imagens/foto.png" alt="Salão para reservar"></a>
+             <a href="salao.php"><img src="imagens/salao.png" alt="Salão para reservar"></a> 
+             <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"> </a>        
+        </article>
+    </section>
+   
+    <section>
+        <article class="dj">
             <h2>DJs Para Eventos</h2>
             <p>Também temos DJs para diferentes tipos de eventos.</p>
-            <a href="dj.php"><img src="imagens/dj-nelasta.png" alt="Dj Nelasta"></a> <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"> </a> <a href="dj.php"><img src="imagens/dj-malvado.png" alt="Fotográfo"></a>
-        </div>
-    </main>
-    
-    <main>
-        <div class="fotografo">
+            <a href="dj.php"><img src="imagens/dj-nelasta.png" alt="Dj Nelasta"></a> 
+            <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"></a> 
+            <a href="dj.php"><img src="imagens/dj-malvado.png" alt="Fotográfo"></a>
+        </article>
+    </section>
+       
+    <section>
+        <article class="fotografo">
             <h2>Fotográfos Para Eventos</h2>
             <p>Temos vários Fotográfos para diferentes tipos de eventos.</p>
-            <a href="fotografo.php"><img src="imagens/fotografo.png" alt="Fotográfos"></a> <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"> </a> <a href="fotografo.php"><img src="imagens/fotografo.png" alt="Fotográfo"></a>
-       </div>
-    </main>
-
+            <a href="fotografo.php"><img src="imagens/fotografo.png" alt="Fotográfos"></a> 
+            <a href="destaques.php"><img src="imagens/foto.png" alt="Imagens de eventos"></a> 
+            <a href="fotografo.php"><img src="imagens/fotografo.png" alt="Fotográfo"></a>
+        </article>
+    </section>
+    
     <?php include ("rodape.php");?>
