daf8331bad402e9ca4ef78525be685005ddc8fc2
Filipe Camarata <filipeedvandro942@gmail.com>
2022-11-18 14:04:15 +0100

    Adicionei os links do index para reserva

--- index.php
+++ index.php
@@ -38,26 +38,67 @@
    
 <article>
         <h2>Salões Para Eventos</h2>
-        
-        <a href="salao.php"><img src="imagens/royal03.png" alt="Salão para reservar"></a>
-        <a href="salao.php"><img src="imagens/alegria02.png" alt="Salão para reservar"></a> 
-        <a href="destaques.php"><img src="imagens/esmeralda01.png" alt="Imagens de eventos"> </a>        
+        <div class="linha espaco">
+            <a href="salao.php"><img src="imagens/royal03.png" alt="Salão para reservar"></a>
+            <p class="paragrafos">S.F Royal Plaza</p>
+            <a href="descricao-salao-1.php" class="botao">Reservar</a>
+        </div> 
+
+        <div class="linha">
+             <a href="salao.php"><img src="imagens/alegria02.png" alt="Salão para reservar"></a> 
+             <p class="paragrafos">S.F Quinta Da Alegria</p>
+             <a href="#" class="botao">Reservar</a>
+        </div>   
+
+        <div class="linha">
+            <a href="destaques.php"><img src="imagens/esmeralda01.png" alt="Imagens de eventos"></a> 
+            <p class="paragrafos">S.F Esmeralda</p>
+            <a href="descricao-salao-2.php" class="botao">Reservar</a>
+        </div>  
+              
 </article>
    
 <article>
     <h2>DJs Para Eventos</h2>
-    
-    <a href="dj.php"><img src="imagens/nelasta02.png" alt="Dj Nelasta"></a> 
-    <a href="dj.php"><img src="imagens/malvado04.png" alt="Dj Malvado"></a> 
-    <a href="dj.php"><img src="imagens/zaques04.png" alt="Fotográfo"></a>
+    <div class="linha espaco">
+        <a href="dj.php"><img src="imagens/nelasta02.png" alt="Dj Nelasta"></a>
+        <p class="paragrafos">DJ Nelasta</p>
+        <a href="descricao-dj-1.php" class="botao">Reservar</a>
+        
+    </div> 
+
+    <div class="linha"> 
+        <a href="dj.php"><img src="imagens/malvado04.png" alt="Dj Malvado"></a> 
+        <p class="paragrafos">DJ Malvado</p>
+        <a href="descricao-dj-2.php" class="botao">Reservar</a>
+    </div> 
+
+    <div class="linha">
+        <a href="dj.php"><img src="imagens/zaques04.png" alt="Fotográfo"></a>
+        <p class="paragrafos">DJ Zaques Bantwini</p>
+        <a href="descricao-dj-3.php" class="botao">Reservar</a>
+    </div> 
 </article>
     
 <article>
     <h2>Fotográfos Para Eventos</h2>
-    
-    <a href="fotografo.php"><img src="imagens/fotografo01.png" alt="Fotográfos"></a> 
-    <a href="fotografo.php"><img src="imagens/fotografo04.png" alt="Imagens de eventos"></a> 
-    <a href="fotografo.php"><img src="imagens/fotografo05.png" alt="Fotográfo"></a>
+    <div class="linha espaco">
+        <a href="fotografo.php"><img src="imagens/fotografo01.png" alt="Fotográfos"></a>
+        <p class="paragrafos"> Ft. Amaral Oliveira</p>
+        <a href="descricao-fotografo-1.php" class="botao">Reservar</a>
+    </div> 
+
+    <div class="linha"> 
+        <a href="fotografo.php"><img src="imagens/fotografo04.png" alt="Imagens de eventos"></a>
+        <p class="paragrafos"> Ft. Ricardo Hamilton</p>
+        <a href="#" class="botao">Reservar</a>
+    </div>  
+
+    <div class="linha">
+        <a href="fotografo.php"><img src="imagens/fotografo05.png" alt="Fotográfo"></a>
+        <p class="paragrafos"> Ft. Jonatas António</p>
+        <a href="#" class="botao">Reservar</a>
+    </div> 
 </article>
     
     
